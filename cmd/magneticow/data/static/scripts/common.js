69f67d427e071dbc432b4f31ff4b0214f690010d
Michał Gątkowski <gatkowski.michal@gmail.com>
2022-08-05 01:17:28 +0200

    Show exact time of discovery

--- cmd/magneticow/data/static/scripts/common.js
+++ cmd/magneticow/data/static/scripts/common.js
@@ -28,7 +28,10 @@ function humaniseDate(unixTime) {
     return (new Date(unixTime * 1000)).toLocaleDateString("en-GB", {
         day: "2-digit",
         month: "2-digit",
-        year: "numeric"
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
     });
 }
 
@@ -37,7 +40,7 @@ function humaniseDate(unixTime) {
 function myFetch(url, options) {
     if (options == null) options = {}
     if (options.credentials == null) options.credentials = 'same-origin'
-    return fetch(url, options).then(function(response) {
+    return fetch(url, options).then(function (response) {
         if (response.status >= 200 && response.status < 300) {
             return Promise.resolve(response)
         } else {
@@ -60,26 +63,25 @@ Date.prototype.getWeek = function (dowOffset) {
     /* getWeek() was developed by Nick Baicoianu at MeanFreePath: http://www.meanfreepath.com */
 
     dowOffset = 1;
-    let newYear = new Date(this.getFullYear(),0,1);
+    let newYear = new Date(this.getFullYear(), 0, 1);
     let day = newYear.getDay() - dowOffset; //the day of week the year begins on
     day = (day >= 0 ? day : day + 7);
     let daynum = Math.floor((this.getTime() - newYear.getTime() -
-        (this.getTimezoneOffset()-newYear.getTimezoneOffset())*60000)/86400000) + 1;
+        (this.getTimezoneOffset() - newYear.getTimezoneOffset()) * 60000) / 86400000) + 1;
     let weeknum;
     // if the year starts before the middle of a week
-    if(day < 4) {
-        weeknum = Math.floor((daynum+day-1)/7) + 1;
-        if(weeknum > 52) {
-            nYear = new Date(this.getFullYear() + 1,0,1);
-            nday = nYear.getDay() - dowOffset;
+    if (day < 4) {
+        weeknum = Math.floor((daynum + day - 1) / 7) + 1;
+        if (weeknum > 52) {
+            var nYear = new Date(this.getFullYear() + 1, 0, 1);
+            var nday = nYear.getDay() - dowOffset;
             nday = nday >= 0 ? nday : nday + 7;
             /*if the next year starts before the middle of
               the week, it is week #1 of that year*/
             weeknum = nday < 4 ? 1 : 53;
         }
-    }
-    else {
-        weeknum = Math.floor((daynum+day-1)/7);
+    } else {
+        weeknum = Math.floor((daynum + day - 1) / 7);
     }
     return weeknum;
 };
